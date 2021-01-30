e4c1e2c8f84b2eccde770ef53d4a77fe4bce36dd
Cosmin Gherghe <hello@cosmingherghe.dev>
2021-01-30 17:38:09 +0000

    Add exceptions if file not found or processing issues

--- GSONandJava/src/dev/cosmingherghe/Main.java
+++ GSONandJava/src/dev/cosmingherghe/Main.java
@@ -1,13 +1,16 @@
 package dev.cosmingherghe;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import dev.cosmingherghe.entity.Pet;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -28,9 +31,29 @@ public class Main {
             System.out.println("Name: " + name + "\nAge: " + age);
 
             //Compute Array Pets that contains Objects
-            
+            List<Pet> petList = new ArrayList<>();
+
+            JsonArray jsonArrayOfPets = fileObject.get("pets").getAsJsonArray();
+            for (JsonElement petElement : jsonArrayOfPets) {
+                //get Json Pet Object
+                JsonObject petJsonObject = petElement.getAsJsonObject();
+
+                //Extract Data
+                String petName = petJsonObject.get("name").getAsString();
+                String petColor = petJsonObject.get("color").getAsString();
+
+                //Create Pet
+                Pet pet = new Pet(petName, petColor);
+                petList.add(pet);
+            }
+
+            System.out.println("All pets: " + petList);
 
         } catch (IOException e) {
+            System.err.println("Error: File not found!");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Error: processing input file!");
             e.printStackTrace();
         }
     }
--- GSONandJava/src/dev/cosmingherghe/entity/Pet.java
+++ GSONandJava/src/dev/cosmingherghe/entity/Pet.java
@@ -1,2 +1,37 @@
-package dev.cosmingherghe.entity;public class Pet {
+package dev.cosmingherghe.entity;
+
+public class Pet {
+    private String name;
+    private String color;
+
+    public Pet(){}
+
+    public Pet(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
